b80d14d08593f9cb39034867fdadb9c6a7f25d78
balachandar341 <balachandar341@gmail.com>
2015-12-14 07:39:03 +0000

    initial subnet

---
+++ subnet.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import sys
+import subprocess
+import csv
+
+former, sys.stdout = sys.stdout, open('o.csv', 'w')
+
+hostname = sys.argv[1]
+#hostname=raw_input("Enter the Host ID or Host Name here : ")
+#hostname = "10.0.2.15" #example
+print (riesponse = os.system("fping -g "+hostname))
+#print response
+
+results,sys.stdout = sys.stdout, former
+results.close()
+#and then check the response...
+#if a == 0:
+#if hostname == 0:
+ # print hostname, 'is up!'
+#else:
+ # print hostname, 'is down!'
