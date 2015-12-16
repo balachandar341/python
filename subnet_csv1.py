2d2816fe845e299a9d94d18a466e99781250266f
balachandar341 <balachandar341@yahoo.com>
2015-12-16 10:44:32 +0530

    Update subnet_csv1.py

--- subnet_csv1.py
+++ subnet_csv1.py
@@ -6,10 +6,10 @@ import csv
 def main():
         hostname = sys.argv[1]
         cmd = ['fping', '-g', hostname]
-	former, sys.stdout = sys.stdout, open('files/subnet_output/output.csv', 'w+')
-        with open('files/output.csv', 'w+') as out:
+	former, sys.stdout = sys.stdout, open('output.csv', 'w+')
+        with open('output.csv', 'w+') as out:
    		subprocess.call(cmd, stdout=out)
-        with open('files/output.csv', 'r') as inp:
+        with open('output.csv', 'r') as inp:
                 for line in inp:
                 	print line.replace("is","").replace("unreachable","free").replace("alive","use").replace("  "," ").replace(" ",",")
 	results,sys.stdout = sys.stdout, former
